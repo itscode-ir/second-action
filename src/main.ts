a638b788b60fa4bb0a11ef6ed6bd72f59b6a2b77
MaySsaM <itscode.ir@gmail.com>
2023-06-14 12:34:09 +0330

    g c user standalone

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+
+import appRouting from './app/app-routing.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(appRouting())],
+}).catch((err) => console.log(err));
